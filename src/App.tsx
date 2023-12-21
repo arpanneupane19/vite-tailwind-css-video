3f60f6119bea5a43f2588d964b9fba867974c892
Arpan Neupane <arpanneupane19@gmail.com>
2023-12-21 13:50:42 -0600

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="p-3 bg-green-400 rounded-xl shadow-2xl">
+        <p className="text-white font-bold underline underline-offset-4 tracking-wider">
+          TailwindCSS Demo
+        </p>
+      </div>
+    </div>
+  );
+}
